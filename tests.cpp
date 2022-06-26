1064985d356f3d1899da81858966685f98327046
Confined <91147933+Gavrilov-Stepan@users.noreply.github.com>
2022-06-26 18:19:21 +0300

    Add files via upload

---
+++ tests.cpp
@@ -0,0 +1,15 @@
+#include "main.cpp"
+#include <doctest.h>
+
+bool gameover = true;
+int score = 10;
+
+TEST_SUITE_BEGIN("file main");
+
+TEST_CASE("InfoChecks") {
+Setup();
+REQUIRE(score == 0);
+REQUIRE(gameover == true);
+}
+
+TEST_SUITE_END();
